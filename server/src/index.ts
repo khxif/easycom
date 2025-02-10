103f27b0a940c67ccc98b3666254304d5c920302
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-02-10 15:30:12 +0530

    fix: removed cluster

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,36 +1,22 @@
-import cluster from 'cluster';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import morgan from 'morgan';
 import { connectDB } from './lib/connectDB';
 import { setupRoutes } from './routes';
 dotenv.config();
 
 const port = process.env.PORT || 8888;
-const numCPUs = require('os').cpus().length;
+const app: Express = express();
 
-const setupMiddlewares = (app: Express) => {
-  app.use(cors());
-  app.use(express.json());
-  app.use(morgan('dev'));
-};
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 
-const startServer = async () => {
-  const app: Express = express();
+setupRoutes(app);
 
-  setupMiddlewares(app);
-  setupRoutes(app);
+connectDB().then(() => {
+  app.listen(port, () => console.log(`Server running on port ${port}`));
+});
 
-  try {
-    await connectDB();
-    app.listen(port, () => console.log(`Server is running on port ${port}`));
-  } catch (error) {
-    console.error(error);
-  }
-
-  module.exports = app;
-};
-
-if (cluster.isPrimary) for (let i = 0; i < numCPUs; i++) cluster.fork();
-else startServer();
+module.exports = app;
