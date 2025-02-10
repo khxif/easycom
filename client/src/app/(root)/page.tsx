5a4cde8d70baa8a99eb75ddacd53ff748eab9d42
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-02-10 13:53:10 +0530

    refactor: products card refactor

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -2,24 +2,20 @@
 
 import { Loading } from '@/components/core/loading';
 import { ProductCard } from '@/components/core/product-card';
-import { useGetProducts } from '@/hooks/queries';
+import { useGetMyFavorites, useGetProducts } from '@/hooks/queries';
+import { useAuthStore } from '@/stores/auth-store';
 import { useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default function HomePage() {
   const [limit, setLimit] = useState(10);
+  const user = useAuthStore(state => state.user);
+
   const { data, isLoading } = useGetProducts(limit);
   const products = data?.data as Product[];
 
-  // const scrollRef = useRef<number>(0);
-  // useEffect(() => {
-  //   window.scrollTo(0, scrollRef.current);
-  // }, [data]);
+  const { data: favorites } = useGetMyFavorites(user?._id as string);
 
-  const handleNext = () => {
-    // scrollRef.current = window.scrollY;
-    setLimit(prev => prev + 10);
-  };
   return (
     <main className="max-w-7xl mx-auto py-6 px-4 w-full h-full">
       <section>
@@ -27,12 +23,23 @@ export default function HomePage() {
           <InfiniteScroll
             dataLength={data?.meta}
             hasMore={data?.meta?.total > limit}
-            next={handleNext}
+            next={() => setLimit(prev => prev + 10)}
             loader={<h4>Loading...</h4>}
-            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 md:gap-5 h-full w-full"
+            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 md:gap-5 h-full w-full pb-5"
           >
             {products?.map(product => (
-              <ProductCard key={product._id} product={product} />
+              <ProductCard
+                key={product._id}
+                productId={product._id}
+                name={product?.name}
+                description={product?.description}
+                price={product?.price}
+                image_url={product?.image_url}
+                isFavorite={favorites?.favorites
+                  ?.map((fav: Product) => fav._id)
+                  .includes(product._id)}
+                userId={user?._id as string}
+              />
             ))}
           </InfiniteScroll>
         ) : (
--- client/src/components/core/product-card.tsx
+++ client/src/components/core/product-card.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import { useAddFavoritesMutation, useRemoveFavoritesMutation } from '@/hooks/mutations';
-import { useGetMyFavorites } from '@/hooks/queries';
-import { useAuthStore } from '@/stores/auth-store';
 import { useQueryClient } from '@tanstack/react-query';
 import { ShoppingCartIcon } from 'lucide-react';
 import Image from 'next/image';
@@ -11,19 +9,33 @@ import { toast } from 'sonner';
 import { Button } from '../ui/button';
 import { Card, CardContent, CardFooter } from '../ui/card';
 
-export function ProductCard({ product }: { product: Product }) {
-  const queryClient = useQueryClient();
-  const user = useAuthStore(state => state.user);
+interface ProductCardProps {
+  productId: string;
+  name: string;
+  description: string;
+  price: number;
+  image_url: string;
+  isFavorite: boolean;
+  userId: string;
+}
 
-  const { data: favorites } = useGetMyFavorites(user?._id as string);
-  const isFavorite = favorites?.favorites?.map((fav: Product) => fav._id).includes(product._id);
+export function ProductCard({
+  name,
+  description,
+  price,
+  image_url,
+  isFavorite,
+  productId,
+  userId,
+}: ProductCardProps) {
+  const queryClient = useQueryClient();
 
   const { mutateAsync: addFavoriteMutation } = useAddFavoritesMutation();
   const { mutateAsync: removeFavoriteMutation } = useRemoveFavoritesMutation();
 
   const addFavorites = async () => {
     try {
-      await addFavoriteMutation({ userId: user?._id as string, productId: product._id });
+      await addFavoriteMutation({ userId, productId });
 
       queryClient.invalidateQueries({ queryKey: ['favorites'] });
     } catch (error) {
@@ -34,7 +46,7 @@ export function ProductCard({ product }: { product: Product }) {
 
   const removeFavorites = async () => {
     try {
-      await removeFavoriteMutation({ userId: user?._id as string, productId: product._id });
+      await removeFavoriteMutation({ userId, productId });
 
       queryClient.invalidateQueries({ queryKey: ['favorites'] });
     } catch (error) {
@@ -48,15 +60,15 @@ export function ProductCard({ product }: { product: Product }) {
         <Image
           width={140}
           height={140}
-          src={product?.image_url}
-          alt={product?.name}
+          src={image_url}
+          alt={name}
           className="aspect-square size-48 w-full object-cover rounded-t-lg"
           loading="lazy"
         />
 
         <div className="flex flex-col space-y-0.5">
           <span className="flex items-center justify-between">
-            <h4 className="text-lg font-medium">{product?.name}</h4>
+            <h4 className="text-lg font-medium">{name}</h4>
             <Heart
               isActive={isFavorite}
               onClick={isFavorite ? removeFavorites : addFavorites}
@@ -66,14 +78,12 @@ export function ProductCard({ product }: { product: Product }) {
               animationDuration={0.1}
             />
           </span>
-          <p className="truncate text-muted-foreground">{product?.description}</p>
+          <p className="truncate text-muted-foreground">{description}</p>
         </div>
       </CardContent>
       <CardFooter className="flex justify-between items-center mt-2">
         <p className="font-medium">
-          {new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR' }).format(
-            product?.price,
-          )}
+          {new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR' }).format(price)}
         </p>
 
         <Button className="flex space-x-2" size="sm">
