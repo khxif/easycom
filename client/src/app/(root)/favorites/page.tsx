8e59c7727197aee366c24f16d961dad69b729cdf
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-02-10 14:19:10 +0530

    feat: completed favorites page

--- client/src/app/(root)/favorites/page.tsx
+++ client/src/app/(root)/favorites/page.tsx
@@ -1,3 +1,35 @@
+'use client';
+
+import { ProductCard } from '@/components/core/product-card';
+import { useGetMyFavorites } from '@/hooks/queries';
+import { useAuthStore } from '@/stores/auth-store';
+
 export default function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+  const user = useAuthStore(state => state.user);
+  const { data: favorites } = useGetMyFavorites(user?._id as string);
+
+  return (
+    <main className="max-w-7xl mx-auto py-6 px-4 w-full h-full flex flex-col space-y-6">
+      <h1 className="text-2xl font-semibold">My Favorites</h1>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 md:gap-5 w-full pb-5">
+        {favorites?.favorites?.map((favorite: Product) => (
+          <ProductCard
+            key={favorite._id}
+            productId={favorite._id}
+            name={favorite?.name}
+            description={favorite?.description}
+            price={favorite?.price}
+            image_url={favorite?.image_url}
+            isFavorite={true}
+            userId={user?._id as string}
+          />
+        ))}
+      </section>
+      {favorites?.favorites?.length === 0 && (
+        <p className="text-center text-gray-500 text-lg mx-auto">
+          You don&apos;t have any favorites yet!
+        </p>
+      )}
+    </main>
+  );
 }
